36a8ae69662c7476da413261ac55fc8dc7c0387d
yokovaski <erwinlenting@outlook.com>
2016-03-29 11:29:36 +0200

    nog steeds init spul

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,5 +1,22 @@
 package controller;
 
-public class Controller {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import model.Model;
+import view.View;
+
+public class Controller implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
+		if(source instanceof Model){
+			//do stuff
+		}
+		else if(source instanceof View){
+			//do stuff
+		}
+	}
 
 }
--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -1,5 +1,22 @@
 package model;
 
-public class Model {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
+public class Model {
+	private ArrayList<ActionListener> actionListenerList = new ArrayList<ActionListener>();
+	
+	public void addActionListener(ActionListener actionListener){
+		actionListenerList.add(actionListener);
+	}
+	
+	public void notifyListeners(){
+		processEvent( new ActionEvent( this, ActionEvent.ACTION_PERFORMED, null));
+	}
+	
+	private void processEvent(ActionEvent e){
+		for( ActionListener l : actionListenerList)
+			l.actionPerformed( e );
+	}
 }
