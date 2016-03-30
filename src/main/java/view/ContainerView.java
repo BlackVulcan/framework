81dcbf453c5a79e93b6b38226d2ab28df340bf65
Alexander Hatzmann <ahatzmann@gmail.com>
2016-03-30 10:37:43 +0200

    Add to the container instead of the JFrame

--- src/main/java/view/ContainerView.java
+++ src/main/java/view/ContainerView.java
@@ -1,36 +1,34 @@
 package view;
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import java.awt.Dimension;
+public class ContainerView extends JFrame implements View {
+    private static final long serialVersionUID = 1L;
+    JPanel container;
+    ArrayList<JButton> buttons = new ArrayList<>();
 
-public class ContainerView extends JFrame implements View{
-	private static final long serialVersionUID = 1L;
-	JPanel container;
-	ArrayList<JButton> buttons = new ArrayList<>();
-	public ContainerView() {		
-		container = new JPanel();
-		this.getContentPane().add(container, BorderLayout.CENTER);
-		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
+    public ContainerView() {
+        container = new JPanel();
+        this.getContentPane().add(container, BorderLayout.CENTER);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setMinimumSize(new Dimension(400, 300));
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public ArrayList<JButton> getButtons(){
-		return buttons;
-	}
-	
-	public void showView(JPanel panel){
-		getContentPane().add(panel, BorderLayout.CENTER);
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public ArrayList<JButton> getButtons() {
+        return buttons;
+    }
+
+    public void showView(JPanel panel) {
+        container.add(panel, BorderLayout.CENTER);
+    }
 }
